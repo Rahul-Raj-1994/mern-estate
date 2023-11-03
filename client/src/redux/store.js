86441245211575c3016df6d29bb817caad0ed355
Rahul Raj <me.rahulraj07@gmail.com>
2023-11-03 13:11:24 +0530

    added redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,28 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from 'redux-persist';
 import userReducer from './user/userSlice.js';
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({ user: userReducer });
+
+// We need to persist the store so that the data or the user information is not lost when the page is refreshed.
+// Hemce,the user information is persisted in the localStoage of the browser.
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // configure or create a store and then provide this store to the enire react application in index.js file
 export const store = configureStore({
-    reducer: { user: userReducer },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+export const persistor = persistStore(store);
